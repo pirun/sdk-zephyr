89a9d9b844c737dc0e31bb8452cb4c4a018dc85b
Pirun Lee <pirun.lee@nordicsemi.no>
2024-01-12 12:01:04 +0800

    drivers: add support for nRF54L ECB
    nRF54L change ECB data structure from ECBDATAPTR to EasyDMA.
    Reassign ECB pointers right before encryption to ensure proper values.
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>a
    Signed-off-by: Pirun Lee <pirun.lee@nordicsemi.no>

--- drivers/crypto/crypto_nrf_ecb.c
+++ drivers/crypto/crypto_nrf_ecb.c
@@ -8,6 +8,7 @@
 #include <zephyr/crypto/crypto.h>
 #include <zephyr/logging/log.h>
 #include <hal/nrf_ecb.h>
+#include <zephyr/sys/byteorder.h>
 
 #define DT_DRV_COMPAT nordic_nrf_ecb
 
@@ -17,6 +18,10 @@
 LOG_MODULE_REGISTER(crypto_nrf_ecb, CONFIG_CRYPTO_LOG_LEVEL);
 
 struct ecb_data {
+#if defined(CONFIG_SOC_SERIES_NRF54LX)
+	nrf_vdma_job_t in_job;
+	nrf_vdma_job_t out_job;
+#endif /* CONFIG_SOC_SERIES_NRF54LX */
 	uint8_t key[ECB_AES_KEY_SIZE];
 	uint8_t cleartext[ECB_AES_BLOCK_SIZE];
 	uint8_t ciphertext[ECB_AES_BLOCK_SIZE];
@@ -46,6 +51,35 @@ static int do_ecb_encrypt(struct cipher_ctx *ctx, struct cipher_pkt *pkt)
 		memcpy(drv_state.data.cleartext, pkt->in_buf,
 		       ECB_AES_BLOCK_SIZE);
 	}
+#if defined(CONFIG_SOC_SERIES_NRF54LX)
+	uint32_t *key_reverse = (uint32_t *)drv_state.data.key;
+
+	/* AES key order should be reversed on nRF54L */
+	sys_mem_swap(key_reverse, ECB_AES_KEY_SIZE);
+	nrf_ecb_key_set(NRF_ECB00, (uint32_t *)drv_state.data.key);
+
+	if (IS_ENABLED(CONFIG_CRYPTO_NRF_ECB_MULTIUSER)) {
+		nrf_ecb_in_ptr_set(NRF_ECB00, &drv_state.data.in_job);
+		nrf_ecb_out_ptr_set(NRF_ECB00, &drv_state.data.out_job);
+	}
+
+	drv_state.data.in_job.size = pkt->in_len;
+	drv_state.data.out_job.size = pkt->in_len;
+	nrf_ecb_event_clear(NRF_ECB00, NRF_ECB_EVENT_END);
+	nrf_ecb_event_clear(NRF_ECB00, NRF_ECB_EVENT_ERROR);
+	nrf_ecb_task_trigger(NRF_ECB00, NRF_ECB_TASK_START);
+	while (!(nrf_ecb_event_check(NRF_ECB00, NRF_ECB_EVENT_END) ||
+		 nrf_ecb_event_check(NRF_ECB00, NRF_ECB_EVENT_ERROR))) {
+	}
+	if (nrf_ecb_event_check(NRF_ECB00, NRF_ECB_EVENT_ERROR)) {
+		LOG_ERR("ECB operation error");
+		return -EIO;
+	}
+
+#else
+	if (IS_ENABLED(CONFIG_CRYPTO_NRF_ECB_MULTIUSER)) {
+		nrf_ecb_data_pointer_set(NRF_ECB, &drv_state.data);
+	}
 
 	nrf_ecb_event_clear(NRF_ECB, NRF_ECB_EVENT_ENDECB);
 	nrf_ecb_event_clear(NRF_ECB, NRF_ECB_EVENT_ERRORECB);
@@ -57,19 +91,35 @@ static int do_ecb_encrypt(struct cipher_ctx *ctx, struct cipher_pkt *pkt)
 		LOG_ERR("ECB operation error");
 		return -EIO;
 	}
+#endif /* CONFIG_SOC_SERIES_NRF54LX */
 	if (pkt->out_buf != drv_state.data.ciphertext) {
 		memcpy(pkt->out_buf, drv_state.data.ciphertext,
 		       ECB_AES_BLOCK_SIZE);
 	}
+
 	pkt->out_len = pkt->in_len;
+
 	return 0;
 }
 
 static int nrf_ecb_driver_init(const struct device *dev)
 {
 	ARG_UNUSED(dev);
+#if defined(CONFIG_SOC_SERIES_NRF54LX)
+	/* Magic number in nRF54L15 OPS v0.5b page 251 8.7.2 EasyDMA */
+	#define NRF_VDMA_ATTRIBUTE_ECB 11
 
+	drv_state.data.in_job.p_buffer = (uint8_t *)&drv_state.data.cleartext;
+	drv_state.data.in_job.size = 0;
+	drv_state.data.in_job.attributes = NRF_VDMA_ATTRIBUTE_ECB;
+	drv_state.data.out_job.p_buffer = (uint8_t *)&drv_state.data.ciphertext;
+	drv_state.data.out_job.size = 0;
+	drv_state.data.out_job.attributes = NRF_VDMA_ATTRIBUTE_ECB;
+	nrf_ecb_in_ptr_set(NRF_ECB00, &drv_state.data.in_job);
+	nrf_ecb_out_ptr_set(NRF_ECB00, &drv_state.data.out_job);
+#else
 	nrf_ecb_data_pointer_set(NRF_ECB, &drv_state.data);
+#endif /* CONFIG_SOC_SERIES_NRF54LX */
 	drv_state.in_use = false;
 	return 0;
 }
